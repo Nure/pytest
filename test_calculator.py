914978d8994a241d7e9158924ddb2079528628b5
Nure Siddiq <nalamsiddiq@gmail.com>
2026-06-19 22:55:52 +0600

    Add intentional test fail

--- test_calculator.py
+++ test_calculator.py
@@ -2,9 +2,7 @@ import pytest
 from calculator import add, subtract, multiply, divide
 
 def test_add():
-    assert add(2, 3) == 5
-    assert add(-1, 1) == 0
-    assert add(0, 0) == 0
+    assert add(2, 3) == 7
 
 def test_subtract():
     assert subtract(5, 3) == 2
